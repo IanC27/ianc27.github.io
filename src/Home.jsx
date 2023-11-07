b055cb70bfa292d93d92162e909a5b8c3b957734
Ian Cowan <icowan@ucsc.edu>
2023-11-07 13:01:41 -0800

    added to summary

--- src/Home.jsx
+++ src/Home.jsx
@@ -47,8 +47,15 @@ export default function Home() {
         </Typography>
         </NameTag>
         <Box sx={{}}>
-          <Typography sx={{}} variant="body1">
-            {"Hi!, I'm a game designer and programmer, and these are some of my projects."}
+          <Typography sx={{maxWidth: 1000}} variant="body1">
+            {`
+            Hi! I'm a game designer and programmer, and these are some of my projects.
+            I would describe myself as a generalist; I like to solve all manner of problems from all areas of development: 
+            from low-level systems debugging to scripting core gameplay.
+            But in particular, I enjoy creating useful and highly re-usable components to make things like 
+            level design easier. I also like UX design, improving interfaces and player experience to be accessible and
+            easy to undestand.
+            `}
           </Typography>
         </Box>
       </Stack>
